e5335a1b6f698c7194fe9caa74206d4fe337c3c7
Joseph Ligier <littel.jo@gmail.com>
2019-12-20 17:43:16 +0100

    Improve code

--- asi.py
+++ asi.py
@@ -4,42 +4,48 @@ import requests
 from catt.api import CattDevice
 
 url= "https://api.arretsurimages.net/api/public/rss/emission/arret-sur-images"
-feed = feedparser.parse(url)
-
-links_list = []
-url_list = []
 
 class MyHTMLParser(HTMLParser):
+    url_list = []
+
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             if attr[0] == 'href' and attr[1].startswith('http://v42.arretsurimages.net/fichiers') and attr[1].endswith('.mp4'):
-                url_list.append(attr[1])
+                self.url_list.append(attr[1])
 
+def find_ip():
+    return "192.168.0.43"
 
 def cast_video(ip, video):
     cast = CattDevice(ip_addr=ip)
     cast.play_url(video, resolve=True, block=True)
 
-for i, post in enumerate(feed.entries):
-  date = "(%d/%02d/%02d)" % (post.published_parsed.tm_year,\
-    post.published_parsed.tm_mon, \
-    post.published_parsed.tm_mday)
-  
+def print_video_asi(feed):
+    links_list = []
+    for i, post in enumerate(feed.entries):
+      date = "(%d/%02d/%02d)" % (post.published_parsed.tm_year,\
+        post.published_parsed.tm_mon, \
+        post.published_parsed.tm_mday)
+      links_list.append(post.link)
+      print(f"{i} {date}: {post.title}")
+    return links_list
 
-  links_list.append(post.link)
-  print(f"{i} {date}: {post.title}")
+
+feed = feedparser.parse(url)
+links_list = print_video_asi(feed)
 
 number = int(input("Choice: "))
 
 url = links_list[number]
+
 r = requests.get(url)
 
 parser = MyHTMLParser()
 parser.feed(r.text)
 
-if len(url_list) > 0:
-    print(url_list[0])
-    cast_video("192.168.0.43", url_list[0])
+if len(parser.url_list) > 0:
+    ip = find_ip()
+    cast_video(ip, parser.url_list[0])
 else:
     print("video unavailable")
 
